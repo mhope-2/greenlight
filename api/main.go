5b2d37d42126b1ae323cbed9ce9fa3ff5f2a5b3e
mhope <mhope@npontu.com>
2021-06-05 01:13:26 +0000

    server: server setup

--- api/main.go
+++ api/main.go
@@ -1,6 +1,62 @@
 package main
-import "fmt"
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+// config struct
+type config struct {
+	port int
+	env string
+}
+
+// application struct
+type application struct {
+	config config
+	logger *log.Logger
+}
+
 
 func main() {
-	fmt.Println("Hello world!")
+
+	// Declare an instance of the config struct.
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// Initialize a new logger which writes messages to the standard out stream,
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Declare an instance of the application struct, containing the config struct and the logger.
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Declare a new servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// Declare a HTTP server with some sensible timeout settings
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+
+	// Start the HTTP
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }
