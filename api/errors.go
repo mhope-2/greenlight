485e363b4a3c2ddb5eb31784f1c11d2130b74836
mhope <mhope@npontu.com>
2021-06-07 09:28:18 +0000

    refactor: updated internal/validator and createmovie handler

--- api/errors.go
+++ api/errors.go
@@ -49,9 +49,15 @@ func (app *Application) methodNotAllowedResponse(w http.ResponseWriter, r *http.
 }
 
 
-// bad Reques tResponse
+// bad Request Response
 func (app *Application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
+
+
+// failed Validation Response
+func (app *Application) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
+	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors) }
+
 
 
